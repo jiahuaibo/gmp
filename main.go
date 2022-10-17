8417f3b092edcee2bd10cb6dda7a94d2f4d619b9
贾怀波 <jiahuaibo@MacBookPro.local>
2022-10-17 23:04:10 +0800

    v0.0

--- main.go
+++ main.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 	"os"
 	"runtime/trace"
+	"time"
 )
 
-func main() {
+func main1() {
 	f, err := os.Create("trace.out")
 	if err != nil {
 		panic(err)
@@ -18,4 +19,11 @@ func main() {
 	}
 	defer trace.Stop()
 	fmt.Println("Hello World")
+}
+
+func main() {
+	for i := 0; i < 5; i++ {
+		time.Sleep(time.Second)
+		fmt.Println("Hello World")
+	}
 }
